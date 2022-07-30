af9872530a30d149c9e668cc30d8a3cccd0a8ed2
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2022-07-30 23:59:56 +0900

    Add files via upload

---
+++ C/codeup1366.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for (int i=1; i<=n; i++,puts(""))
+        for (int j=1; j<=n; j++)
+        {
+            if(j==1 || i==1 || i==n || j==n || i==j || i==n-j+1 || n/2+1==i || n/2+1==j) printf("*");
+            else printf(" ");
+        }
+}
