fa1570cb5366cc5b33e551fb0f97cdfaf2dbc23b
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2022-12-20 13:59:21 +0900

    221220

--- C/DataStructure&Algorithm/DataStrucutre/Graph/depth_first_search(DFS).c
+++ C/DataStructure&Algorithm/DataStrucutre/Graph/depth_first_search(DFS).c
@@ -3,9 +3,11 @@
 #define MAX_VERTICES 50
 #define TRUE 1
 #define FALSE 0
+// 0 부터 시작 안한다
+
 //그래프 노드(정점)구조체 정의
 typedef struct GraphNode{
-	int vertex;
+	int vertex; // p[x] 값
 	struct GraphNode* link;
 }GraphNode;
 //각 정점의 인접 정점 리스트 시작주소를 포인터 배열 구조체로 정의
@@ -18,8 +20,8 @@ int visited[MAX_VERTICES]; // 50
 void init(GraphType* g){
 	int v; //v는 정점 
 	g->n=0; //그래프의 정점수 초기화 
-	for (v = 0;v < MAX_VERTICES;v++){
-		g->adj_list[v]=NULL;	
+	for (v = 0;v < MAX_VERTICES;v++){ 
+		g->adj_list[v]=NULL;	// 처음에는 null 로 초기화
 	}
 } 
 void insert_vertex(GraphType* g, int v){//정점 개수 1증가 
@@ -27,25 +29,24 @@ void insert_vertex(GraphType* g, int v){//정점 개수 1증가
 	   printf("그래프: 정점의 개수 초과");
 		return; 
 	}
-	g->n++;//[문]정점개수 증가
+	g->n++; //[문]정점개수 증가
 } 
 //간선 삽입 연산
-void insert_edge(GraphType* g, int u, int v) { //u ->v1->...
+void insert_edge(GraphType* g, int u, int v) { //u = 0(0번정점), v = 1
 	GraphNode  *node, *p;
-	if (u>=g->n || v>=g->n){
+	if (u>=g->n || v>=g->n){ // 배열의 크기를 벗어날 때
 			printf("그래프: 정점번호 오류\n");
 		return; 
 	}
 	node = (GraphNode*)malloc(sizeof(GraphNode));
 	node->vertex= v;
-	if (g->adj_list[u] ==NULL ){
+	if (g->adj_list[u] == NULL ){
 		node->link=g->adj_list[u];//링크를 정점의 포인터배열 시작주소로 저장 
 		g->adj_list[u]=node;//새로운 노드 주소로 갱신 
 	}
-	else{
-		for(p=g->adj_list[u];p->link!=NULL;p=p->link);
-			
-		p->link = node;//p는 리스트의 마지막 노드
+	else{ //1번째의 주소있음(null 아님)
+		for(p=g->adj_list[u];p->link!=NULL;p=p->link); // 계속 다음 노드의 시작 주소를 인용 / 맨 끝으로
+		p->link = node;//p는 리스트의 마지막 노드 / for문으로 마지막으로 돌림
 		node->link = NULL;
 	}
 }
@@ -60,14 +61,13 @@ void print_adj_list(GraphType* g){
 		printf("\n");
 	}
 }
-void dfs_list(GraphType* g, int v){
-	GraphNode* w;
-	visited[v] = TRUE; //방문배열에 정점 방문 저장 / 여기 다녀감(방문했다)
+void dfs_list(GraphType* g, int v){ // v = 1
+	GraphNode* w; // 1번과 연결된 노드를 방물할 때 쓰기
+	visited[v] = TRUE; //방문배열에 정점 방문 저장 / 여기 다녀감(방문했다? 그럼 출력)
 	printf("정점 %d-> ", v);//정점출력
-	for(w=g->adj_list[v];w!=NULL;w=w->link){
-		if (visited[w->vertex]== 0){//미방문
+	for(w=g->adj_list[v];w!=NULL;w=w->link){ // adj_list[1] 에 0의 주소가 있음
+		if (visited[w->vertex]== 0){//미방문 
 			dfs_list(g, w->vertex);//정점 탐색호출
-			
 		}
 	}
 }
@@ -90,7 +90,7 @@ int main(){
 		
 	printf("깊이 우선탐색 시작 정점 입력 \n");
 	scanf("%d",&n);
-	dfs_list(g, n);
+	dfs_list(g, n); // n = 1
 	free(g);	
 	return 0;
 	
